7ef55f4e9a6c8ec68bcb20d167570d28c737c5a8
Guillaume R <theelectronwill@gmail.com>
2018-08-08 00:04:05 +0200

    [F] Polls entries and answers

---
+++ app/Models/PollEntry.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/** Model for polls entries, ie available answers to a thread's poll */
+class PollEntry extends Model
+{
+    public $timestamps = false;
+    protected $table = 'polls_entries';
+
+    /**
+     * Gets the thread that offers the poll that contains this entry.
+     */
+    public function thread()
+    {
+        return $this->belongsTo('App\Models\Thread');
+    }
+
+    /**
+     * Gets all the users that chose this entry as an answer to the poll.
+     */
+    public function users_answered()
+    {
+        return $this->belongsToMany('App\Models\User', 'polls_answers', 'entry_id', 'user_id');
+    }
+}
