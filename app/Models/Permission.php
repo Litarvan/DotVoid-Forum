05397558c7d59edd02d4898e66f5c446767f0f87
Guillaume R <theelectronwill@gmail.com>
2018-08-07 14:43:20 +0200

    [F] Permissions and roles models

---
+++ app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    /**
+     * Gets the roles that have this permission.
+     */
+    public function roles()
+    {
+      return $this->belongsToMany('App\Models\Role', 'roles_permissions', 'perm_id', 'role_id');
+    }
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    /**
+     * Gets the users with this role.
+     */
+    public function users()
+    {
+      return $this->hasMany('Apps\Models\User');
+    }
+
+    /**
+     * Gets the role's permissions.
+     */
+    public function permissions()
+    {
+      return $this->belongsToMany('App\Models\Permission', 'roles_permissions', 'role_id', 'perm_id');
+    }
+}
